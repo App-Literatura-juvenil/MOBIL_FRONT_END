98a1672c6fdae75c14533b771f8aa8870098afa0
Drake8V <drake8v@gmail.com>
2020-10-21 18:33:29 -0500

    Actualizaciones pagina principal
    Se crea idea de diseño para pagina principal, se arregla header y se crea footer.

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,12 +4,14 @@ import { HomePage } from './home.page';
 import { LibrosComponent } from './libros/libros.component';
 import { OrientacionComponent } from './orientacion/orientacion.component';
 import { PresentacionComponent } from './presentacion/presentacion.component';
+import { ViewCapituloComponent } from './view-capitulo/view-capitulo.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePage },
   { path: 'orientacion', component: OrientacionComponent },
   { path: 'presentacion', component: PresentacionComponent },
   { path: 'libros', component: LibrosComponent},
+  { path: 'view-capitulo', component: ViewCapituloComponent},
   {
     path: '',
     redirectTo: 'home',
