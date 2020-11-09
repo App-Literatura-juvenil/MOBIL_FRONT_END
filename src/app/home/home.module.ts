74b941a6e3b9b6ae1984ac5a3d45f6ea836e0c44
Alejowilches0112 <cawilchesfe@poligran.edu.co>
2020-11-08 21:53:09 -0500

    se eliminan declaraciones repetidas

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -10,17 +10,6 @@ import { OrientacionComponent } from './orientacion/orientacion.component';
 import { LibrosComponent } from './libros/libros.component';
 import { AutorComponent } from './autor/autor.component';
 import { ListaAutorComponent } from './lista-autor/lista-autor.component'
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { IonicModule } from "@ionic/angular";
-import { FormsModule } from "@angular/forms";
-import { HomePage } from "./home.page";
-
-import { HomePageRoutingModule } from "./home-routing.module";
-import { PresentacionComponent } from "./presentacion/presentacion.component";
-import { OrientacionComponent } from "./orientacion/orientacion.component";
-import { LibrosComponent } from "./libros/libros.component";
-import { AutorComponent } from "./autor/autor.component";
 import { BiografiaComponent } from './biografia/biografia.component';
 
 @NgModule({
