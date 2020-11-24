2c1aae9d99ff802808610fa1b31aac4ccd8cd246
Alejowilches0112 <cawilchesfe@poligran.edu.co>
2020-11-24 12:39:48 -0500

    se eliminan audios e imagenes que no se requieren en la app

--- src/app/services/question-service.service.ts
+++ src/app/services/question-service.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { from, of } from 'rxjs';
-import ListQuestionBook1 from 'src/assets/Question/QuestionDB.json';
 import { Question } from '../home/models/question.model';
 
 @Injectable({
@@ -11,11 +10,7 @@ import { Question } from '../home/models/question.model';
 
 export class QuestionServiceService{
     constructor(private http: HttpClient){}
-        
-    public findByIdBookQuestionsAnswer(idBook:Number){
-      //return this.http.get(`${environment.urlApi}questionAnswer/${idBook}`);
-      return of(<Question[]>ListQuestionBook1);
-    }
+    
     public findByIdBookQuestions(idBook:Number){
       return this.http.get(`${environment.urlApi}question/questionBook/${idBook}`);
     }
